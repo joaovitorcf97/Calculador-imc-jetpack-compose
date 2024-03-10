14da33e7fcadb5e96e33ea3652a7e496aa389ae9
João vitor <joaovitorcfdev@gmail.com>
2024-03-09 23:09:14 -0300

    resentando campos

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -13,9 +13,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -33,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -80,7 +85,22 @@ fun CalculadoraIMC() {
                 Text(text = "Calculadora IMC", color = WHITE)
             }, colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = LIGHT_BLUE
-            )
+            ),
+            actions = {
+                IconButton(
+                    onClick = {
+                        peso = ""
+                        altura = ""
+                        textoResultado = ""
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = "Botão de resetar todos os campos",
+                        tint = WHITE
+                    )
+                }
+            }
         )
     }) {
         Column(
