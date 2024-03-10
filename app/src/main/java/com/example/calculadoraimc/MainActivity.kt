7c3cad9c658847977bdc45afbf3af51159274fa1
João vitor <joaovitorcfdev@gmail.com>
2024-03-09 22:17:01 -0300

    criando tela principal

---
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -0,0 +1,172 @@
+package com.example.calculadoraimc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.calculadoraimc.ui.theme.CalculadoraIMCTheme
+import com.example.calculadoraimc.ui.theme.DARK_BLUE
+import com.example.calculadoraimc.ui.theme.LIGHT_BLUE
+import com.example.calculadoraimc.ui.theme.WHITE
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculadoraIMCTheme {
+                CalculadoraIMC()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CalculadoraIMC() {
+    var peso by remember {
+        mutableStateOf("")
+    }
+
+    var altura by remember {
+        mutableStateOf("")
+    }
+
+    var textoResultado by remember {
+        mutableStateOf("X")
+    }
+
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Text(text = "Calculadora IMC", color = WHITE)
+            }, colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = LIGHT_BLUE
+            )
+        )
+    }) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+                .padding(16.dp, 38.dp, 16.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Calcular de IMC",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                color = LIGHT_BLUE
+            )
+
+            OutlinedTextField(value = peso,
+                onValueChange = { it ->
+                    peso = it
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 16.dp, 0.dp, 0.dp),
+                label = {
+                    Text(text = "Peso (Kg)")
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    cursorColor = LIGHT_BLUE,
+                    focusedBorderColor = LIGHT_BLUE,
+                ),
+                textStyle = TextStyle(DARK_BLUE, 16.sp),
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+
+            OutlinedTextField(value = altura,
+                onValueChange = { it ->
+                    altura = it
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 16.dp, 0.dp, 0.dp),
+                label = {
+                    Text(text = "Altura")
+                },
+                colors = OutlinedTextFieldDefaults.colors(
+                    cursorColor = LIGHT_BLUE,
+                    focusedBorderColor = LIGHT_BLUE,
+                ),
+                textStyle = TextStyle(DARK_BLUE, 16.sp),
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 24.dp, 0.dp, 24.dp)
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = LIGHT_BLUE
+                )
+            ) {
+                Text(
+                    text = "Calcular",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Text(
+                text = textoResultado,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = DARK_BLUE
+            )
+        }
+    }
+}
+
+//@Preview
+//@Composable
+//fun AppPreview() {
+//    CalculadoraIMC()
+//}
